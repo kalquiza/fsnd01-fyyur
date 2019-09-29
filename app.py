8dc95fe115e69448ef4946284dcdc6709db2a80f
Kristoffer Alquiza <kalquiza@gmail.com>
2019-09-29 14:48:09 -0700

    Implement search_venues
    Search on Venue.name column with case-insensitive partial string search

--- app.py
+++ app.py
@@ -152,17 +152,26 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  # implement search on artists with partial string search. Ensure it is case-insensitive.
+
+  search='%'+request.form.get('search_term')+'%'
+  venues = Venue.query.filter(Venue.name.ilike(search)).all()
+  count= Venue.query.filter(Venue.name.ilike(search)).count()
+  data=[]
+  
+  for venue in venues:    
+    data.append({
+      "id": venue.id,
+      "name": venue.name,
+      "num_upcoming_shows": Show.query.filter(Show.venue_id==venue.id, Show.start_time>datetime.today()).count()
+    })
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
+    "count": count,
+    "data": data
+  }  
+
+
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
