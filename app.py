d78e25fca07e4ed56a32ae46e153d5850d6311c4
Kristoffer Alquiza <kalquiza@gmail.com>
2019-09-29 00:36:22 -0700

    Implement '/artists' route

--- app.py
+++ app.py
@@ -309,16 +309,13 @@ def edit_venue_submission(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
+  artists = Artist.query.all()
+  data=[]
+  for artist in artists:
+    data.append({
+      "id": artist.id,
+      "name": artist.name
+    })
   return render_template('pages/artists.html', artists=data)
 
 @app.route('/artists/search', methods=['POST'])
