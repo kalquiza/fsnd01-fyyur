27f552c8888039b7708c39f410b33c92a70d78c1
Kristoffer Alquiza <kalquiza@gmail.com>
2019-09-29 14:36:54 -0700

    Implement search_artists
    Search on Artist.name column with case-insensitive partial string search

--- app.py
+++ app.py
@@ -326,17 +326,27 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+
+  search='%'+request.form.get('search_term')+'%'
+  artists = Artist.query.filter(Artist.name.ilike(search)).all()
+  count= Artist.query.filter(Artist.name.ilike(search)).count()
+  data=[]
+  
+  for artist in artists:    
+    data.append({
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": Show.query.filter(Show.artist_id==artist.id, Show.start_time>datetime.today()).count()
+    })
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+    "count": count,
+    "data": data
+  }  
+
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
